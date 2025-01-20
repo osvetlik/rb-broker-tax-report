e1a6d003016aed1191b1f951689ef72b5748fe53
Ondřej Světlík <ondrej.svetlik@yoso.fi>
2025-01-20 19:23:26 +0100

    Generated project

---
+++ src/main/java/info/svetlik/rb/report/RbBrokerTaxReportApplication.java
@@ -0,0 +1,13 @@
+package info.svetlik.rb.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbBrokerTaxReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbBrokerTaxReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/svetlik/rb/report/RbBrokerTaxReportApplicationTests.java
@@ -0,0 +1,13 @@
+package info.svetlik.rb.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbBrokerTaxReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
